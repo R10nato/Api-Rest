c86c6bf269976877b3df624d6f2af0983bbb3ab9
Renato Lopes <renatolopes.2278@gmail.com>
2021-04-25 21:23:42 -0400

    acrescentei configuracao em urls e views

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hospital.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(' ', home),
 ]
